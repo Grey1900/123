9ea64851371d9f5dfe02f734ec930b5593179c6a
Grey1900 <36457305+Grey1900@users.noreply.github.com>
2024-01-13 12:25:42 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream»
+
+void UpdatelfGreater (int& a, int& b){
+  if(a > b){
+	  b = a;
+  }
+}
+
+int main){
+  int a, b;
+  std::cin > a >> b;
+  
+	UpdatelfGreater(a, b);
+  std::cout << b;
+}
